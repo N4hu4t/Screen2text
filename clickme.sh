1a26f3dccdf73f2bfffa6f1e17434855b3981e22
N4hu4t <nahuatlolmeca@protonmail.com>
2024-11-25 19:48:23 +0800

    Create clickme.sh
    use me

---
+++ clickme.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+xdotool click 4  # Scroll up
+sleep 0.1 
+
+xdotool click 5  # Scroll down
+sleep 0.1  
+
+xdotool click 1  # Left-click
+sleep 0.1  
+# More examples:
+#xdotool mousemove 500 500
+#xdotool mousedown 1
+#xdotool mousemove_relative 0 100
+#xdotool mouseup 1
